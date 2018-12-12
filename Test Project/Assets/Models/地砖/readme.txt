1614e8716b370041959cf4ee963abe7c95c69c8e
Incubator4 <aries0robin@gmail.com>
2018-12-12 18:00:27 +0800

    Start Scene Version

---
+++ Test Project/Assets/Models/地砖/readme.txt
@@ -0,0 +1,4 @@
+地砖清单：
+1.教室：tile_old ,nomal ,new
+2.特殊：tile_chemistry ,computer,basis
+3.走廊:  tile_path
---
+++ Test Project/Assets/Script/StartGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour {
+
+	public Button startButton;
+	// Use this for initialization
+	void Start () {
+		Button btn = startButton.GetComponent<Button>();
+		btn.onClick.AddListener(delegate { OnStartGame("Game"); });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnStartGame(String scene) {
+		SceneManager.LoadScene(scene);
+	}
+}
